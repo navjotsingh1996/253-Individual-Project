7aee76e07c5ae5045776c2be36adb27bab7219c1
navjotsingh1996 <navjotsingh1996@aol.com>
2018-02-03 23:10:04 -0800

    adding all assets

---
+++ Project/Assets/Scripts/GunController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+
+    public GameObject BulletSpawn;
+    public GameObject Bullet;
+    public GameObject Explosion;
+
+    int bulletSpeed = 2000;
+
+    Animation shoot;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /* need to integrate with oculus here */
+		if (Input.GetMouseButtonDown(0))
+        {
+            shoot = gameObject.GetComponent<Animation>();
+            shoot.Play();
+            Invoke("stopShooting", shoot.clip.length);
+            GameObject b = Instantiate(Bullet, BulletSpawn.transform.position, Quaternion.identity);
+            b.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * bulletSpeed);
+        }
+	}
+    void stopShooting()
+    {
+        shoot.Stop();
+    }
+}
---
+++ Project/Assets/Scripts/GunData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunData : MonoBehaviour {
+
+	public enum gunType { handgun, ar, lmg, rocket, sniper, smg };
+    public enum fireRate { semi, multi };
+
+    struct guns
+    {
+        gunType type;
+        string name;
+        fireRate fr;
+        int clipLength;
+        float reloadTime;
+    }
+}
